77b3c5cf94cbba2f04465493c6299d11fdb0d532
Delano Maloney <ddmbossdesigns@gmail.com>
2015-02-17 21:49:53 -0400

    Indentation change

--- ddm-post-format-support.php
+++ ddm-post-format-support.php
@@ -1,18 +1,18 @@
 <?php 
-    /**
-     * Plugin Name: DDM Post Format Support
-     * Plugin URI: https://delanomaloney.com/
-     * Description: A simple plugin that overrides the default post format support of themes, without the need to edit the theme's files.
-     * Version: 1.0.0
-     * Author: Delano Maloney
-     * Author URI: https://delanomaloney.com/
-     * License: GPL3
-     */
+/**
+ * Plugin Name: DDM Post Format Support
+ * Plugin URI: https://www.delanomaloney.com/
+ * Description: A simple plugin that overrides the default post format support of themes, without the need to edit the theme's files.
+ * Version: 1.0.0
+ * Author: Delano Maloney
+ * Author URI: https://www.delanomaloney.com/
+ * License: GPL3
+ */
 
-function add_post_formats() {
+function ddm_add_post_formats() {
     add_theme_support( 'post-formats', array( 'aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video' ) );
     }
  
-add_action( 'after_setup_theme', 'add_post_formats', 20 );
+add_action( 'after_setup_theme', 'ddm_add_post_formats', 20 );
 
 ?>
