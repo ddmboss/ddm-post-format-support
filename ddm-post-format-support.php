503cbd52be0142025f23cabaded4116f75532760
Delano Maloney <4204999+ddmboss@users.noreply.github.com>
2022-02-16 14:01:26 +0800

    Update ddm-post-format-support.php

--- ddm-post-format-support.php
+++ ddm-post-format-support.php
@@ -1,12 +1,16 @@
 <?php 
     /**
+     ---
      * Plugin Name: DDM Post Format Support
      * Plugin URI: https://github.com/ddmboss/ddm-post-format-support
+     * GitHub Plugin URI: https://github.com/ddmboss/ddm-post-format-support
      * Description: A simple plugin that overrides the default post format support of themes, without the need to edit the theme's files.
      * Version: 1.0.1
+     * License: GPL2
+     ---
      * Author: ddmboss designs
      * Author URI: https://www.ddmbossdesigns.com/
-     * License: GPL2
+     ---
      */
 
 function ddm_add_post_formats() {
